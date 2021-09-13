f163a896264ee3c545840186f4bcdb4b346bd252
Sohel Rana <73012378+fsmdsohel@users.noreply.github.com>
2021-09-14 05:02:19 +0600

    All overall done

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const loadProducts = () => {
 };
 loadProducts();
 
+let productData = {};
+
 // show all product in UI
 const showProducts = (products) => {
     const allProducts = products.map((pd) => pd);
@@ -16,21 +18,51 @@ const showProducts = (products) => {
         div.classList.add("product");
         div.innerHTML = `
             <div class="single-product">
-            <div>
-            <img class="product-image" src=${image}></img>
-            </div>
-            <h3>${product.title}</h3>
+            <div class="image-container">
+             <img class="product-image" src=${image}></img>
+             </div>
+           <div class="card-content">
+            <div class="card-header" title="${product.title}">${product.title}</div>
             <p>Category: ${product.category}</p>
             <p>Total-Rating: ${product.rating.count}</p>
-            <p>Avarage-Rating: ${product.rating.rate}</p>
+            <div class="product-${product.id}">
+                <div class="stars-outer">
+                    <div class="stars-inner"></div>
+                </div>
+                <span class="number-rating">${product.rating.rate}</span>
+            </div>
             <h2>Price:$ ${product.price}</h2>
-            <div class="card-btn"><button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary2">add to cart</button>
-            <button id="details-btn" class="btn btn-secondery2">Details</button></div>
+          <div class="card-btn"><button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary2">add to cart</button>
+          <button onclick="displayDetails('${product.id}')" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-secondery2">Details</button></div>
         </div>
         `;
         document.getElementById("all-products").appendChild(div);
     }
+    getRating(products);
+    productData = products;
 };
+
+// display modal product details
+function displayDetails(index) {
+    console.log(event.target);
+    const product = productData.find(
+        (product) => product.id === parseInt(index)
+    );
+    document.getElementById(
+        "product-body"
+    ).innerHTML = `<div class="p-3 d-flex  justify-content-center">
+    <img src="${product.image}" alt=""
+      style="height: 150px; width: auto;">
+    </div>
+    <p><strong>Description:</strong> ${product.description}</p>
+    <p><strong>Category:</strong> ${product.category}</p>`;
+
+    document.getElementById(
+        "product-footer"
+    ).innerHTML = `<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+  <button data-bs-dismiss="modal" onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary2">add to cart</button>`;
+}
+
 let count = 0;
 const addToCart = (id, price) => {
     count = count + 1;
@@ -43,7 +75,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
     const element = document.getElementById(id).innerText;
-    const converted = parseInt(element);
+    const converted = parseFloat(element);
     return converted;
 };
 
@@ -83,5 +115,35 @@ const updateTotal = () => {
         getInputValue("price") +
         getInputValue("delivery-charge") +
         getInputValue("total-tax");
-    document.getElementById("total").innerText = grandTotal;
+    document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+// get ratings
+function getRating(products) {
+    const starsTotal = 5;
+    products.forEach((product) => {
+        // get percentage
+        const starPercentage = (product.rating.rate / starsTotal) * 100;
+
+        // Round to nearest 10
+        const starPercentageRounded = `${
+            Math.round(starPercentage / 10) * 10
+        }%`;
+
+        // Set width of stars inner to percentage
+        document.querySelector(
+            `.product-${product.id} .stars-inner`
+        ).style.width = starPercentageRounded;
+    });
+}
+
+// show my cart
+const showMyCard = () => {
+    document.getElementById("my-cart").classList.toggle("show");
+};
+
+// show cart
+document.getElementById("show-cart").onclick = showMyCard;
+
+// hide cart
+document.getElementById("hide-cart").onclick = showMyCard;
